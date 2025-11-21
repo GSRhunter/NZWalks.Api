9168c4918859896cfddbab892157908a59703467
Shankar Gogula <shankargogula324@gmail.com>
2025-11-21 10:51:26 +0530

    Add project files.

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet ]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            { new  Region
+            {   Id = Guid.NewGuid(),
+                Name = "Auckland region",
+                Code = "AKL",
+                RegionImageUrl = "https://www.pexels.com/photo/green-trees-under-white-sky-2790395/"
+            },
+            new  Region
+            {   Id = Guid.NewGuid(),
+                Name = "Newk region",
+                Code = "NKR",
+                RegionImageUrl = "https://www.pexels.com/photo/green-trees-under-white-sky-2790395/"
+            },
+            };
+            return Ok(regions);
+
+        }
+
+    }
+}
---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{   
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet("GetAllStudents")]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "shankar", "raju", "venkatesh" };
+
+            return Ok(studentNames);    
+
+
+        }
+
+    }
+}
